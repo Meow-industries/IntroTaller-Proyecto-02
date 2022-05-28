26183d38ee50be2f7b53f7d314e6c5f1253d5cbb
Yherland E <yhelizondo@estudiantec.cr>
2022-05-28 02:51:32 -0600

    proceso de creacion de ventanas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import PhotoImage
+from tkinter import PhotoImage, GROOVE
 import pygame
 
 class GraphicUserInterface(tk.Tk):
@@ -10,7 +10,6 @@ class GraphicUserInterface(tk.Tk):
         tk.Tk.__init__(self, *args, **kwargs)# constructor 
          
         self.__configureWindow() 
-        
         # creating a container
         self.__container = tk.Frame(self) 
         self.__container.pack(side = "top", fill = "both", expand = True)
@@ -30,7 +29,7 @@ class GraphicUserInterface(tk.Tk):
             self.frames[screenFrame] = frame
   
             frame.grid(row = 0, column = 0, sticky ="nsew")
-  
+    
         self.showFrame(MainMenu)
 
     """showing the frame"""
@@ -42,33 +41,38 @@ class GraphicUserInterface(tk.Tk):
         """ Main window configuration """
 
         self.title("BATTLESHIP")
-        self.geometry("800x600+198+100")
-        self.iconbitmap('media/icon.ico') 
+        self.geometry("800x600+300+60")
+        self.iconbitmap('media/icon.ico') #TODO: Revisar compatibilidad con mac
         self.resizable(False, False)
-
+        
 class MainMenu(tk.Frame):
     def __init__(self, parent, controller): #constructor
         tk.Frame.__init__(self, parent) #constructor
         self.__initComponents()
     
-    def __initComponents(self):
+    def __initComponents(self,):
         """Widget calling"""
         self.__setupCanvas()
         self.__setupBackground()
-        self.__setupButtons()
         self.__setupMusic()
+        self.__setupEntry()
+        self.__setupButton()
+
+    def __setupButton(self):
+        playButton = tk.Button(self, text="Play", command= lambda : controller.show_frame(GameScreen))
+        playButton.place(x=10, y=10)
     
     def __setupCanvas(self):
         """Canvas configuration"""
         self.__menuCanva = tk.Canvas(self, width= 800, height= 600, borderwidth=0)
         self.__menuCanva.place(x=0, y=0)
+
+
     def __setupBackground(self):
         """Background configuration"""
 
-        #Using global variables to show the images
-        global bgImg
+        global bgImg #Global variable to show the image
 
-        #Background Image configuration
         bgImg = PhotoImage(file= "media/menu.png")
         bgLabel = tk.Label(self.__menuCanva, image = bgImg)
         bgLabel.place(x=0, y=0)
@@ -83,19 +87,28 @@ class MainMenu(tk.Frame):
         pygame.mixer.music.load("sound/menuTrack.mp3")
         pygame.mixer.music.play(loops=-1) #Play the song while the user is in MainMenu
 
-    def __setupButtons(self):
-        """Buttons setup"""
-        playBtn = tk.Button(self.__menuCanva, text = "Play")
-        playBtn.config(width=30, height=3)
-        playBtn.place(x=300, y=400)
-
     def __setupEntry(self):
         """Entry setup"""
         self.__userNameInput = tk.StringVar()
-        self.__userNameInput.set("")
-        #voy por aqui, falta poner el input como tal
+        self.__userNameInput.set("Ingrese su nombre")
+        nameEntry = tk.Entry(self.__menuCanva, textvariable=self.__userNameInput, font=("Helvetica", 10, "bold" ))
+        nameEntry.config(width=30)
+        nameEntry.place(x=300, y=300)
+
 class GameScreen(tk.Frame):
-    """creating the animation & fibonacci screen"""
+    """Game Screen"""
     def __init__(self, parent, controller): #constructor
-        tk.Frame.__init__(self, parent)#constructor 
-        #self.__initComponents()
+        tk.Frame.__init__(self, parent) #constructor 
+        self.__initComponents()
+    
+    def __initComponents(self):
+        self.__setupCanvas()
+
+    def __setupCanvas(self):
+        """Canvas configuration"""
+
+        self.__boatsCanvas = tk.Canvas(self, width=372, height=372,bg="Green")
+        self.__boatsCanvas.place(x=0, y=0)
+
+        self.__gameCanvas = tk.Canvas(self, width=372, height=372, bg="Pink") 
+        self.__gameCanvas.place(x=373, y=0)
